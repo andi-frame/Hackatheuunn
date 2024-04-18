bf73e3e3d5c2ea3c807705c1e890f71edf7ea5bb
andi-frame <andifarhan1094@gmail.com>
2024-04-18 12:16:58 +0700

    feat: add margin to Header logo for alignment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 function Header() {
   return (
     <div className="flex justify-between content-center bg-green-500">
-      <div className="flex m-2 py-1 px-2 rounded-full bg-white">
+      <div className="flex my-2 mx-5 py-1 px-2 rounded-full bg-white">
         <img className="w-10 h-10" src="src\assets\life_token_logo.png" alt="life_token_logo" />
         <img className="w-10 h-10" src="src\assets\life_token_logo.png" alt="life_token_logo" />
         <img className="w-10 h-10" src="src\assets\life_token_logo.png" alt="life_token_logo" />
       </div>
-      <div className="flex m-2 items-center">
+      <div className="flex my-2 mx-5 items-center">
         <img className="w-10 h-10" src="src\assets\maca_logo.png" alt="maca_logo" />
       </div>
     </div>
