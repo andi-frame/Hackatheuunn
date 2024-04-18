68b27f7f9d76227f2068ef0eed9e06b1abd924fa
andi-frame <andifarhan1094@gmail.com>
2024-04-18 14:52:11 +0700

    feat: Complete add layout component Category to Explore

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,5 +1,16 @@
-function Explore(){
+import Category from "./Category";
 
+function Explore() {
+  return (
+    <div className="my-5 ml-5">
+      <h2 className="text-3xl">Explore</h2>
+      <div className="flex">
+        <Category name="All"></Category>
+        <Category name="Fairy Tail"></Category>
+        <Category name="Science"></Category>
+      </div>
+    </div>
+  );
 }
 
 export default Explore;
