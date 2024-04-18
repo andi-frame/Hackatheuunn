2702e36019d83ffe11b7088263c8d3c2dcd3ceb2
andi-frame <andifarhan1094@gmail.com>
2024-04-18 12:26:17 +0700

    feat: start component Insights

--- src/components/Insights.jsx
+++ src/components/Insights.jsx
@@ -1,5 +1,16 @@
 function Insights() {
-  
+  return (
+    <div className="bg-green-500 w-auto h-40 p-3 m-5 rounded-2xl">
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default Insights;
