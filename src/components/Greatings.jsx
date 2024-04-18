7813b5fa86874aaa50d72a0c1830abc83c05680b
andi-frame <andifarhan1094@gmail.com>
2024-04-18 12:13:53 +0700

    feat: done layout component Header and Greetings

--- src/components/Greatings.jsx
+++ src/components/Greatings.jsx
@@ -1,5 +1,12 @@
-function Greatings(){
-
+function Greatings() {
+  return (
+    <div className="bg-blue-500 w-auto h-40 p-3 m-5 flex flex-col rounded-xl justify-end">
+      <span>
+        Hello, <strong>Barru Adi!</strong>
+      </span>
+      <span>How are you?</span>
+    </div>
+  );
 }
 
 export default Greatings;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-function Header(){
-  return (<>
-    
-  </>)
+function Header() {
+  return (
+    <div className="flex justify-between content-center bg-green-500">
+      <div className="flex m-2 py-1 px-2 rounded-full bg-white">
+        <img className="w-10 h-10" src="src\assets\life_token_logo.png" alt="life_token_logo" />
+        <img className="w-10 h-10" src="src\assets\life_token_logo.png" alt="life_token_logo" />
+        <img className="w-10 h-10" src="src\assets\life_token_logo.png" alt="life_token_logo" />
+      </div>
+      <div className="flex m-2 items-center">
+        <img className="w-10 h-10" src="src\assets\maca_logo.png" alt="maca_logo" />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
