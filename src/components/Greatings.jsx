0694dbabe67d26ca64d1771ed1988ddbcc78baf6
andi-frame <andifarhan1094@gmail.com>
2024-04-18 12:25:51 +0700

    feat: change border round to 2xl

--- src/components/Greatings.jsx
+++ src/components/Greatings.jsx
@@ -1,6 +1,6 @@
 function Greatings() {
   return (
-    <div className="bg-blue-500 w-auto h-40 p-3 m-5 flex flex-col rounded-xl justify-end">
+    <div className="bg-blue-500 w-auto h-40 p-3 m-5 flex flex-col rounded-2xl justify-end">
       <span>
         Hello, <strong>Barru Adi!</strong>
       </span>
