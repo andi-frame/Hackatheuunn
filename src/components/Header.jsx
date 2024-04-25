722d0b7ca1c7354b51a00a6960552b91b9ce1bc3
andi-frame <andifarhan1094@gmail.com>
2024-04-25 21:03:48 +0700

    Update image src to import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
+import life_token_logo from "../assets/life_token_logo.png"
+import maca_logo from "../assets/maca_logo.png"
+
 function Header() {
   return (
     <div className="flex justify-between content-center bg-green-500">
       <div className="flex my-2 mx-5 py-1 px-2 rounded-full bg-white">
-        <img className="w-10 h-10" src="src\assets\life_token_logo.png" alt="life_token_logo" />
-        <img className="w-10 h-10" src="src\assets\life_token_logo.png" alt="life_token_logo" />
-        <img className="w-10 h-10" src="src\assets\life_token_logo.png" alt="life_token_logo" />
+        <img className="w-10 h-10" src={life_token_logo} alt="life_token_logo" />
+        <img className="w-10 h-10" src={life_token_logo} alt="life_token_logo" />
+        <img className="w-10 h-10" src={life_token_logo} alt="life_token_logo" />
       </div>
       <div className="flex my-2 mx-5 items-center">
-        <img className="w-10 h-10" src="src\assets\maca_logo.png" alt="maca_logo" />
+        <img className="w-10 h-10" src={maca_logo} alt="maca_logo" />
       </div>
     </div>
   );
